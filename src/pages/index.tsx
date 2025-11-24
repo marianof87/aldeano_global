0dd654bbcb9fda0ffdd39757ae52edbd6f915f4c
marianof87 <mfcapella87@gmail.com>
2025-11-23 22:53:28 -0800

    estilo y background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,12 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
+      <div className='part1'>
       <div className="container py-5">
         <h1 className="mb-4">Plataforma Cultural</h1>
         <p className="lead">
           -ALDEANO GLOBAL ES UN MEDIO DIGITAL-</p>
-          <p className="text">Un espacio de comunicación cultural que aborda múltiples expresiones artísticas, sociales y científicas. Nos dedicamos a la difusión de contenidos que promueven el conocimiento, la reflexión y el diálogo en torno a temas de interés cultural y social. Ofrecemos una variedad de formatos, incluyendo columnas de opinión, podcasts en YouTube y espacios para prensa y difusión.</p>
+          <h5 className='txt'>Un espacio de comunicación cultural que aborda múltiples expresiones artísticas, sociales y científicas. Nos dedicamos a la difusión de contenidos que promueven el conocimiento, la reflexión y el diálogo en torno a temas de interés cultural y social. Ofrecemos una variedad de formatos, incluyendo columnas de opinión, podcasts en YouTube y espacios para prensa y difusión.</h5>
         <div className="row mt-5">
           <div className="col-md-4">
             <div className="card1">
@@ -33,9 +34,38 @@ export default function Home() {
                 <h5 className="card3-title">Prensa & Difusión</h5>
                 <p className="card3-text">Conocé y hacete conocer</p>
               </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div>
+        <div className='part2'>
+      <div>
+        <h5 className='card4'>
+          Aldeano Global surge como un espacio de comunicación cultural que va más allá de la música para abarcar todas las expresiones artísticas y humanísticas. Este proyecto, anteriormente conocido como Estación Mapamundi Radio, se ha transformado en un punto de encuentro para curiosos, soñadores y apasionados de la diversidad cultural. Con un enfoque en el pensamiento crítico y la promoción de valores interculturales, Aldeano Global se consolida como una plataforma dedicada a explorar y difundir las riquezas culturales del mundo.
+El contenido que ofrece incluye una amplia variedad de temáticas, desde la historia del arte hasta la literatura contemporánea, así como la música tradicional de distintas culturas. 
+        </h5>
+      </div>
+      <div>
+        <h5 className='card4'>
+          A través de redes sociales como YouTube, Facebook e Instagram, promueven el análisis profundo de obras literarias, artes visuales y temas actuales. La ética, la responsabilidad y el respeto por la diversidad de pensamiento son pilares fundamentales de su misión, fomentando un diálogo inclusivo y constructivo con sus seguidores.
+Entre sus iniciativas más destacadas se encuentra la recuperación y difusión de tradiciones como la panadería antigua, la realización de entrevistas con artistas, escritores y músicos, y la organización de eventos culturales.
+        </h5>
+      </div>
+      <div>
+        <h5 className='card4'>
+          Asimismo, este medio también se encarga de la difusión sobre exposiciones, conciertos y festivales literarios tanto locales como internacionales, creando un puente entre las distintas expresiones artísticas y su público.
+Aldeano Global también busca construir una comunidad activa y participativa. Invitan a sus seguidores a compartir sus historias, creaciones artísticas, y recomendaciones de libros y música, fortaleciendo el sentido de pertenencia y colaboración. Esta interacción enriquece el espacio y lo convierte en un verdadero crisol de ideas y experiencias culturales.
+        </h5>
+      </div>
+      <div>
+        <h5 className='card4'>
+La promoción de la diversidad cultural y el respeto intercultural es uno de sus objetivos principales. 
+A través de la interacción en redes sociales y la difusión de contenido de calidad, Aldeano Global no solo informa, sino que también inspira a sus seguidores a valorar la riqueza de las culturas del mundo, destacando la importancia del entendimiento mutuo y el intercambio de ideas.
+        </h5>
+        </div>
+      </div>
       </div>
     </Layout>
   );
